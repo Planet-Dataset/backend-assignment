64844ef5823760e77e92db8f071b70d3865a26b0
Alejandro Rodríguez Díaz <jancho@usal.es>
2021-11-11 09:48:23 +0100

    Remove Linux-dependent commands

--- backend.mk
+++ backend.mk
@@ -1,19 +1,3 @@
-# Shell to use for running scripts
-SHELL := $(shell which bash)
-
-# Test if the dependencies we need to run this Makefile are installed
-DOCKER := $(shell command -v docker)
-DOCKER_COMPOSE := $(shell command -v docker-compose)
-deps:
-ifndef DOCKER
-	@echo "Docker is not available. Please install docker"
-	@exit 1
-endif
-ifndef DOCKER_COMPOSE
-	@echo "docker-compose is not available. Please install docker-compose"
-	@exit 1
-endif
-
 DB_USER=root
 DB_PASS=pass
 
